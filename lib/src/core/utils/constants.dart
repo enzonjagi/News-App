270d59629be5e09f277e26a913e8b19be191d5ff
enzonjagi <amosndungo@gmail.com>
2022-03-04 16:06:14 +0300

    Configurations Set

---
+++ lib/src/core/utils/constants.dart
@@ -0,0 +1,13 @@
+import 'dart:core';
+
+const String kMaterialAppTitle = 'News App';
+
+// API constants
+const String kBaseUrl = 'https://newsapi.org';
+
+const String kApiKey = '9b343928ce994596ae3bbf51e0b17120';
+
+// Database Constants
+const String kArticlesTableName = 'articles_table';
+
+const String kDatabaseName = 'app_database_db';
